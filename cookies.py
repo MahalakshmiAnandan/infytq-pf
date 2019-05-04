736b2e6b0969b9b64a97a02bb0c890003bc947c6
MahalakshmiAnandan <47468528+MahalakshmiAnandan@users.noreply.github.com>
2019-05-04 14:02:47 +0530

    Create cookies.py
    To claculate total no of cookies sold in an airport

---
+++ cookies.py
@@ -0,0 +1,8 @@
+no of flights landed=100
+no of flights took off=100
+no of seating capacity in flights landed=185
+no of seating capacity in flights took off=150
+total people in flights took off=no of flights took off*no of seating capacity in flights took off
+total people in flights landed=no of flights took off*(no of seating capacity in flights landed/2)
+total cookies sold=(total people in flights took off+total people in flights landed)*2
+display(total cookies sold)
