8c1e4d8b78a2b677987d49b41c34dabd4f3c31fc
MahalakshmiAnandan <47468528+MahalakshmiAnandan@users.noreply.github.com>
2019-05-08 23:03:45 +0530

    Create leap year list.py

---
+++ leap year list.py
@@ -0,0 +1,9 @@
+def find_leap_years(given_year):
+    count=0
+    list_of_leap_years=[]
+    while(count<15):
+        if(given_year%4==0 and given_year%100!=0 or given_year%400==0):
+            list_of_leap_years.append(given_year)
+            count+=1
+       given_year=given_year+1
+  find_leap_year(1990)
