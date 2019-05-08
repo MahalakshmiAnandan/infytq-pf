a4e0a262dc86fe7c2eb1e6337e747664f2efb121
MahalakshmiAnandan <47468528+MahalakshmiAnandan@users.noreply.github.com>
2019-05-08 23:00:58 +0530

    Create chicken and rabbit count.py

---
+++ chicken and rabbit count.py
@@ -0,0 +1,18 @@
+def solve(heads,legs):
+   error_msg="No solution"
+    chicken_count=0
+    rabbit_count=0
+    c=0
+    flag=1
+    for r in range(heads,-1,-1):
+        if(2*chicken_count+4*rabbit_count==legs):
+            
+           chicken_count=c
+            rabbit_count=rabbit_count
+            break
+        c=c+1
+    if(flag==0):
+       print(chicken_count,rabbit_count)
+    else:
+        print(error_msg)
+solve(38,131)
